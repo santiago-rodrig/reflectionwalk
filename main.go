720a36b4ee8ccd95190973f3dd6278afa7c6e9f4
santiago-rodrig <santo1996.29@gmail.com>
2021-02-19 21:36:38 -0400

    Walk supports channels

--- main.go
+++ main.go
@@ -22,6 +22,10 @@ func Walk(x interface{}, fn func(input string)) {
 		for _, key := range val.MapKeys() {
 			walkValue(val.MapIndex(key))
 		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walkValue(v)
+		}
 	case reflect.String:
 		fn(val.String())
 	}
